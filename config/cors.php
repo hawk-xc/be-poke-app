62786771d800a6420ca585565a5db776ee071651
hawkxc <hawkxc068@gmail.com>
2025-10-04 11:10:32 +0700

    update: update cors

--- config/cors.php
+++ config/cors.php
@@ -16,7 +16,7 @@ return [
     */
 
     // 'paths' => ['*', 'sanctum/csrf-cookie'],
-    'paths' => ['api/*', 'geni/*', 'sanctum/csrf-cookie'],
+    'paths' => ['*'],
 
     'allowed_methods' => ['*'],
 
@@ -33,6 +33,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
